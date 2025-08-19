340f78ee750813805b526cbd6a282b17f52a4ee2
nutcas3 <maurice.nyanja.e@gmail.com>
2025-08-20 00:44:58 +0300

    added modules

--- src-tauri/crates/aether_core/src/engine/mod.rs
+++ src-tauri/crates/aether_core/src/engine/mod.rs
@@ -1,5 +1,9 @@
 pub mod timeline;
 pub mod renderer;
 pub mod video_decoder;
+pub mod integration;
+pub mod timeline_renderer;
 
 pub use video_decoder::{VideoFormat, VideoFrame, MediaInfo, StreamInfo};
+pub use timeline_renderer::TimelineRenderer;
+pub use integration::IntegratedExporter;
