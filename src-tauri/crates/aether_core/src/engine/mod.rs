5495066ee317008f3583bfb908838b0eb1e52070
nutcas3 <maurice.nyanja.e@gmail.com>
2025-08-20 17:09:11 +0300

    added render to be used

--- src-tauri/crates/aether_core/src/engine/mod.rs
+++ src-tauri/crates/aether_core/src/engine/mod.rs
@@ -4,6 +4,8 @@ pub mod video_decoder;
 pub mod integration;
 pub mod timeline_renderer;
 
+
 pub use video_decoder::{VideoFormat, VideoFrame, MediaInfo, StreamInfo};
 pub use timeline_renderer::TimelineRenderer;
 pub use integration::IntegratedExporter;
+pub use renderer::Renderer;
